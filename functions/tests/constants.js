3000d3ea7d67355028b550c810572d06b8b52de9
Isai Lopez <ilopezro@ucsc.edu>
2020-11-18 10:32:41 -0800

    added middleware tests

---
+++ functions/tests/constants.js
@@ -0,0 +1,14 @@
+const functions = {
+  config: () => {
+    return {
+      autocomplete: {
+        auth_token: "token",
+        apikey: "key",
+      },
+    };
+  },
+};
+
+module.exports = {
+  functions,
+};
